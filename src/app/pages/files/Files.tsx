26ca717a021f31a8283132b385b18e620adc5453
Steve James <me@deathbyknowledge.com>
2025-05-05 14:17:49 +0200

    sanitize path

--- src/app/pages/files/Files.tsx
+++ src/app/pages/files/Files.tsx
@@ -5,9 +5,12 @@ import { listFolder } from "./functions";
 
 export async function Files({ ctx, params }: RequestInfo) {
     let path = decodeURIComponent(params.$0);
-    if (!path.endsWith("/")) {
-        path += "/";
+    if (path.endsWith("/")) {
+        path = path.slice(0, -1);
     }
+    const segments = path.split("/").filter(segment => segment && segment !== "");
+    path = `/${segments.join("/")}/`;
+    console.log('path', path);
     const entries = await listFolder(path);
     return (
         <ShellLayout user={ctx.user!}>
